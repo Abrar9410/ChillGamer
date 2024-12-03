83c581afcbb158877b2307073842b16a5b782325
Abrar Shahriar <abrarshahriar9410@gmail.com>
2024-12-04 00:30:25 +0600

    Login & Register pages completed

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './routes/Router.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AuthProvider from './providers/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Router}></RouterProvider>
+    </AuthProvider>
+    <ToastContainer></ToastContainer>
   </StrictMode>,
 )
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MainLayout from "../layouts/MainLayout";
 
 
 const Router = createBrowserRouter([
     {
         path: "/",
-        element: <App></App>
-    }
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/register",
+                element: <Register></Register>
+            },
+        ]
+    },
 ])
 
 export default Router;
